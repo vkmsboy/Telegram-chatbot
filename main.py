16bf00221c73136db9342aed62c8c690cb9f4778
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2023-04-08 20:48:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def init_user(user):  # 初始化用户
         preference_list[str(user.id)]['name'] = user.full_name
         threading.Thread(target=save_preference).start()
 
-updater = telegram.ext.Updater(token=CONFIG['Token'])
+updater = telegram.ext.Updater(token='6298615623:AAHIIBpDQZu4EaS5T2F6gWig34nvP3A7ebw')
 dispatcher = updater.dispatcher
 
 me = updater.bot.get_me()
